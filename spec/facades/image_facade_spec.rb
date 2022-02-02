c9674312dfc8d28bd1c6ddadadb52976284552f7
matthewjholmes <ilookup@gmail.com>
2022-02-02 12:43:19 -0700

    revert to passing test

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -5,7 +5,7 @@ describe ImageFacade, type: :facade do
     describe '.get_background_image' do
       it 'returns an image object' do
         image = ImageFacade.get_background_image('denver,co')
-        expect(image).to be_a(Hash)
+        expect(image).to be_a(Image)
       end
     end
   end
